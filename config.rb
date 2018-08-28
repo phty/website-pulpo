8c1e8a0e0002bd2928d5a6b5c9cc1a8d8e7a7b20
dapavargassa <42623941+dapavargassa@users.noreply.github.com>
2018-08-28 09:42:53 -0500

    map en

--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ activate :autoprefixer do |prefix|
 end
 
 activate :livereload
-activate :i18n, :path => "/langs/:locale/"
+activate :i18n, :path => "/langs/:locale/",:lang_map => { :en => :en, :es => :es}
 set :markdown_engine, :redcarpet
 
 # Layouts
